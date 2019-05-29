acc6fb1bae438d746e7289b7fd4815be171bfc3f
Jamal Osmanov <osmanov230497@gmail.com>
2019-05-29 14:53:37 +0300

    Update LaravelConfigWriter.php

--- src/Facades/LaravelConfigWriter.php
+++ src/Facades/LaravelConfigWriter.php
@@ -6,6 +6,11 @@ namespace BONE\LaravelConfigWriter\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class LaravelConfigWriter
+ * @package BONE\LaravelConfigWriter\Facades
+ * @method static write($key, $value)
+ */
 class LaravelConfigWriter extends Facade
 {
     protected static function getFacadeAccessor()
